33f0ee3afe1a92de9a317da9f09ae217a889b4fc
Rivano553 <rivano.putra@ti.ukdw.ac.id>
2025-02-09 22:44:08 +0700

    Create Latihan1,2B.py

---
+++ Latihan1,2B.py
@@ -0,0 +1,9 @@
+hargaEmas1 = 650000 * 25
+hargaEmas2 = 685000 * 15
+hargaJual = 715000 * 40
+hargaBeli = hargaEmas2 + hargaEmas1
+labal = (hargaJual - hargaBeli) / hargaBeli*100
+laba2 = (hargaJual - hargaBeli)
+print ("Keuntungan adalah")
+print("Rp.", laba2)
+print(round(labal,3), '%')
